26b3486ed49eab634e496ae97d2e1ea38b36fab2
Dan Guim <dcguim@gmail.com>
2014-05-11 19:05:09 -0300

    Head-file setup and clean-up

---
+++ AdjacencyList.h
@@ -0,0 +1,9 @@
+/**** Prototypes  ****/
+void create_list( int startPoint[] );
+
+void create_neightbor ( Node* curr, const int config[] );
+
+void which_neightbors (Node* curr, int neightbors[]);
+
+void free_list(Node* begin);
+/********************/
